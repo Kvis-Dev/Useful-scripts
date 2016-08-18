afe0dfeb2622c846caff5147b6a6d02118bbf7b2
Kvis-Dev <kvis.dev@gmail.com>
2016-08-18 11:28:13 +0300

    venv installer

---
+++ install_venvwrapper.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+vdir=~/venvs
+bfile=~/.bashrc
+
+vw=$( which virtualenvwrapper.sh )
+py3=$( which python3 )
+
+installer="#Kvis-Dev venvwrapper installer"
+
+am_I_sourced()
+{
+	if [ "${FUNCNAME[1]}" = source ]; then
+		if [ "$1" = -v ]; then
+			echo ""
+		fi
+	return 0
+	else
+		if [ "$1" = -v ]; then
+			echo ""
+		fi
+		return 1
+	fi
+}
+
+if am_I_sourced -v; then
+
+	cat $bfile | grep --quiet --regexp="$installer"
+	test=$?
+
+	if [[ $test -eq 0 ]]; then
+		echo "Scrip already installed"
+	else
+		sudo pip3 install -U virtualenv virtualenvwrapper
+
+		export WORKON_HOME="$vdir"
+		export VIRTUALENVWRAPPER_PYTHON="$py3"
+
+		mkdir -p $WORKON_HOME
+
+		printf '\n%s\n%s\n%s\n%s\n' "$installer" "export VIRTUALENVWRAPPER_PYTHON=$py3" "export WORKON_HOME=$vdir" "source $vw" >> $bfile
+
+		source "$vw"
+	fi
+else
+	echo "run this script with 'source $0'"
+fi
