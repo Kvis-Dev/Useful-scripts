a006a7c26de92867f32e77e2dc7c81c2ac500798
OlehBoiko <minace002@gmail.com>
2016-09-27 15:48:45 +0300

    New script automatically create vhosts on Mac OS
    Added new script automatically create vhosts on Mac OS

---
+++ automatMacOS.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import os
+import subprocess
+
+def get_immediate_subdirectories(dir):
+    return [name for name in os.listdir(dir)
+            if os.path.isdir(os.path.join(dir, name))]
+
+def main():
+	path = '/Users/mackrais/Sites/'
+	mod = False
+	for subdirname in get_immediate_subdirectories(path):
+		if (not os.path.exists("/etc/apache2/sites-available/" + subdirname+ ".conf") ):
+			sitefile = open("/etc/apache2/sites-available/" + subdirname+ ".conf", "w")
+
+			if os.path.isfile(os.path.join(path, subdirname, 'yii')):
+				docrootdir = subdirname + '/web'
+			else:
+				docrootdir = subdirname
+
+			sitefile.write("""<VirtualHost *:80>
+ServerName """ + subdirname + """		
+ServerAlias www.""" + subdirname + """		
+DocumentRoot """ + os.path.join(path, docrootdir) + """
+<Location />
+  Require all granted
+</Location> 
+<Directory />
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+</Directory>
+</VirtualHost>""")
+			sitefile.close()
+			print("Creating file /etc/apache2/sites-available/" + subdirname + ".conf");
+			
+			print('Writing to hosts file');
+			hosts = open("/etc/hosts", "a")
+			hosts.write("\n 127.0.0.1 " + subdirname)
+			hosts.close()
+		
+			mod = True
+		else:
+			print subdirname + ' - Ok'
+	
+	
+	print('Restarting apache');
+	os.system('apachectl restart')
+		
+	return 0
+
+if __name__ == '__main__':
+	if os.getuid() == 0:
+		main();
+	else:
+		print 'Need sudo'
+		os.system('sudo ' +  os.path.abspath( __file__ ))
+		
